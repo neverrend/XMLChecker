93761b378661aec5a102b485f9afa01da1f24f01
Nick <neverrend@gmail.com>
2019-07-31 21:14:08 -0400

    Cleaned up a bunch of code. Changed the structure of the output. Now compares codeblocks against the stated count

--- XMLCheck.py
+++ XMLCheck.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3 
 
+## TODO: Put this all in a JSON object and simplify the loop.
+
 import os
 import re
 import sys
@@ -10,54 +12,73 @@ def main():
         sys.exit(1)
     
     if os.path.isfile(sys.argv[1]) == True:
-        fileName = sys.argv[1]
+        fileName    = sys.argv[1]
    
     if sys.platform == "win32":
-        backslash = "\\"
+        backslash   = "\\"
     else:
-        backslash = "/"
+        backslash   = "/"   
 
-    #print("File name: {}".format(fileName))    
-
-    fd = open(fileName, "r")
-    data = fd.read()
-    lines = data.split("\n")
+    fd              = open(fileName, "r")
+    data            = fd.read()
+    lines           = data.split("\n")
     
     # Compiled regexs to check for XSD junk first
-    XSDCheck1 = re.compile("assurance_level=\"-1\"(?=[>])") # assurance_level="-1"
-    XSDCheck2 = re.compile("<([/]|)checklistflaws>") # <checklistflaws></checklistflaws>
-    XSDCheck3_1 = re.compile("(?:^[\s]+<location>)(.*)(?:<\\/location>$)") # Location character # checker
-    XSDCheck3 = re.compile("(?:[\s]+<location>)") # Location character # checker
-    XSDCheck4 = re.compile("<\/location>") # End of location checker
+    XSDCheck1       = re.compile("assurance_level=\"-1\"(?=[>])") # assurance_level="-1"
+    XSDCheck2       = re.compile("<([/]|)checklistflaws>") # <checklistflaws></checklistflaws>
+    XSDCheck3_1     = re.compile("(?:^[\s]+<location>)(.*)(?:<\\/location>$)") # Location character # checker
+    XSDCheck3       = re.compile("(?:[\s]+<location>)") # Location character # checker
+    XSDCheck4       = re.compile("<\/location>") # End of location checker
     
-    flawName = re.compile("(?:<name>)(.*)(?:<\\/name>)") # flaw names
-    flawDesc = re.compile("(?:<description>)(.*)(?:<\\/description>)") # flaw description
-    flawRemdiScore = re.compile("(?:<remediationeffort>)(.*)(?:<\\/remediationeffort>)") # flaw remediation score
-    flawRemdiDesc = re.compile("(?:<remediation_desc>)(.*)(?:<\\/remediation_desc>)") # flaw remediation description
+    flawCodeBlocks  = re.compile("(?:<code>)(.*)(?:<\\/code>)") # Code blocks
+    flawCodeBlock   = re.compile("<code>(?!<\\\\code>)") # Code block
+    flawCount       = re.compile("count=\"(\d)\"") # Instance Count
+    flawDesc        = re.compile("(?:<description>)(.*)(?:<\\/description>)") # flaw description
     flawExploitDesc = re.compile("(?:<exploit_desc>)(.*)(?:<\\/exploit_desc>)") # flaw exploit description
-    flawSevDesc = re.compile("(?:<severity_desc>)(.*)(?:<\\/severity_desc>)") # flaw Severity Description
-    flawNote = re.compile("(?:<note>)(.*)(?:<\\/note>)") # CVSS Score
-    flawInputVec = re.compile("(?:<input_vector>)(.*)(?:<\\/input_vector>)") # Input vector
     flawExploitDiff = re.compile("(?:<exploit_difficulty>)(.*)(?:<\\/exploit_difficulty>)") # Exploit difficulty
-    flawCodeBlocks = re.compile("(?:<code>)(.*)(?:<\\/code>)") # Code blocks
+    flawInputVec    = re.compile("(?:<input_vector>)(.*)(?:<\\/input_vector>)") # Input vector
+    flawName        = re.compile("(?:<name>)(.*)(?:<\\/name>)") # flaw names
+    flawNote        = re.compile("(?:<note>)(.*)(?:<\\/note>)") # CVSS Score
+    flawRemdiDesc   = re.compile("(?:<remediation_desc>)(.*)(?:<\\/remediation_desc>)") # flaw remediation description
+    flawRemdiScore  = re.compile("(?:<remediationeffort>)(.*)(?:<\\/remediationeffort>)") # flaw remediation score
+    flawSevDesc     = re.compile("(?:<severity_desc>)(.*)(?:<\\/severity_desc>)") # flaw Severity Description
+    endOfFlaw       = re.compile("<\\/flaw>") # End of a flaw
 
-    print("searching")
-    lineNum = 0
-    name = ""
-    rebuilt = ""
+    print('''
+        ** * * * * * * * * * * * * * * * * * * * **
+        **      *0*      *8*                     **
+        **     \|/________\|/___________         **   
+        **     ALWAYS CHECK THE NEW FILE         **
+        **      THAT IS GENERATED IN MFFC        **
+        **       I AM NOT INFALLABLE ;D          **
+        **                                       **
+        ** * * * * * * * * * * * * * * * * * * * ** 
+    ''')
+    
+    codeCount       = 0
+    count           = 0
+    lineNum         = 0
+    name            = ""
+    rebuilt         = ""
+
     for x in lines:
+        
         line = "" 
+        
         if flawName.search(x):
             name = flawName.search(x)
             name = name.group(1)
             if len(name) == 0:
                 print("Flaw name is missing on line # {}".format(lineNum+1))
+                name = "<!MISSING!>"
+            print("Flaw: {}".format(name))
+        
+        if flawCount.search(x):
+            count = flawCount.search(x).group(1)
 
-        #print(name)
         a = XSDCheck1.search(x)
         if a:
             x = x.replace(a.group(), "")
-            #print(x)
             print("assurance_level found and deleted!")
         
         b = XSDCheck2.search(x)
@@ -65,86 +86,81 @@ def main():
             x = x.replace(b.group(), "")
             print("<checklistflaws> found and deleted!")
 
-        
         c = XSDCheck3.search(x)
         if c:
             orgLine = lineNum
+            
             while range(len(lines[lineNum])):
+                
                 line = line + lines[lineNum]
-                #print(line)
                 end = XSDCheck4.search(lines[lineNum])
                 lineNum += 1
+
                 if end:
                     line = len(list(line))
                     if line > 255:
-                        print("Flaw: {} has to large of a location field. Remove some sites".format(name))
+                        print("- has to large of a location field. Remove some sites")
                     lineNum = orgLine
                     break
 
         d = XSDCheck3_1.search(x, re.MULTILINE)
         if d:
-            print("Flaw: {} is missing locations!".format(name))
+            print("- is missing locations")
     
         e = flawDesc.search(x)
-        if e:
-            if len(e.group()) > 1:
-                if len(e.group(1)) == 0:
-                    print("Flaw: {} is missing a description".format(name))
+        if e and len(e.group()) > 1 and len(e.group(1)) == 0:
+            print("- is missing a description")
 
         f = flawRemdiScore.search(x)
-        if f:
-            if len(f.group()) > 1:
-                if f.group(1) == "0":
-                    print("Flaw: {} is missing a Remediation Score".format(name))
+        if f and len(f.group()) > 1 and f.group(1) == "0":
+            print("- is missing a Remediation Score")
 
         g = flawRemdiDesc.search(x)
-        if g:
-            if len(g.group()) > 1:
-                if len(g.group(1)) == 0:
-                    print("Flaw: {} is missing a Remediation Description".format(name))
+        if g and len(g.group()) > 1 and len(g.group(1)) == 0:
+            print("- is missing a Remediation Description")
 
         h = flawExploitDesc.search(x)
-        if h:
-            if len(h.group()) > 1:
-                if len(h.group(1)) == 0:
-                    print("Flaw: {} is missing a Exploit description".format(name))
+        if h and len(h.group()) > 1 and len(h.group(1)) == 0:
+            print("- is missing a Exploit description")
 
         i = flawSevDesc.search(x)
-        if i:
-            if len(i.group()) > 1:
-                if len(i.group(1)) == 0:
-                    print("Flaw: {} is missing a Severity description".format(name))
+        if i and len(i.group()) > 1 and len(i.group(1)) == 0:
+            print("- is missing a Severity description")
 
         j = flawNote.search(x)
-        if j:
-            if len(j.group()) > 1:
-                if len(j.group(1)) == 0:
-                    print("Flaw: {} is missing a CVSS score".format(name))
+        if j and len(j.group()) > 1 and len(j.group(1)) == 0:
+            print("- is missing a CVSS score")
 
         k = flawInputVec.search(x)
-        if k:
-            if len(k.group()) > 1:
-                if len(k.group(1)) == 0:
-                    print("Flaw: {} is missing a Input Vector".format(name))
+        if k and len(k.group()) > 1 and len(k.group(1)) == 0:
+            print("- is missing a Input Vector")
 
         l = flawExploitDiff.search(x)
-        if l:
-            if len(l.group()) > 1:
-                if l.group(1) == "0":
-                    print("Flaw: {} is missing Exploit Difficulty".format(name))
+        if l and len(l.group()) > 1 and l.group(1) == "0":
+            print("- is missing Exploit Difficulty")
 
         m = flawCodeBlocks.search(x)
         if m:
             x = x.replace(m.group(), "")
-            print("Flaw: {} had empty code blocks that were removed.".format(name))
+            print("- had empty code blocks that were removed.")
+
+        if flawCodeBlock.search(x):
+            codeCount += 1
+       
+        if endOfFlaw.search(x): 
+            if count != codeCount:
+                print("- has a count of \"{}\" and \"{}\" many instances\n"
+                        .format(count, codeCount))
+            codeCount = 0
 
         rebuilt = rebuilt + x + "\n" 
         lineNum += 1
 
+    
     newfilename = fileName.split(backslash)
 
     if sys.platform == "linux":
-        newfilename = backslash.join(newfilename[0:-1]) + (backslash + "new") + newfilename[-1]
+        newfilename = "." + backslash.join(newfilename[0:-1]) + (backslash + "new") + newfilename[-1]
     else:    
         newfilename = backslash.join(newfilename[0:-1]) + "new" + newfilename[-1]
     
